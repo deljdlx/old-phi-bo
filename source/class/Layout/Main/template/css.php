bec6641411f913c90f844c2bafe4d49af99e0fe1
Julien Delsescaux <julien.delsescaux@clickandboat.com>
2017-10-03 16:13:23 +0200

    synchro

--- source/class/Layout/Main/template/css.php
+++ source/class/Layout/Main/template/css.php
@@ -1,18 +1,18 @@
 <!-- Bootstrap -->
-<link href="./vendor/gentelella/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="./vendor/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 <!-- Font Awesome -->
-<link href="./vendor/gentelella/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+<link href="./vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
 <!-- NProgress -->
-<link href="./vendor/gentelella/vendors/nprogress/nprogress.css" rel="stylesheet">
+<link href="./vendor/nprogress/nprogress.css" rel="stylesheet">
 <!-- iCheck -->
-<link href="./vendor/gentelella/vendors/iCheck/skins/flat/green.css" rel="stylesheet">
+<link href="./vendor/iCheck/skins/flat/green.css" rel="stylesheet">
 
 <!-- bootstrap-progressbar -->
-<link href="./vendor/gentelella/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
+<link href="./vendor/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
 <!-- JQVMap -->
-<link href="./vendor/gentelella/vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet"/>
+<link href="./vendor/jqvmap/dist/jqvmap.min.css" rel="stylesheet"/>
 <!-- bootstrap-daterangepicker -->
-<link href="./vendor/gentelella/vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
+<link href="./vendor/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
 
 <!-- Custom Theme Style -->
 <link href="./vendor/gentelella/build/css/custom.min.css" rel="stylesheet">
